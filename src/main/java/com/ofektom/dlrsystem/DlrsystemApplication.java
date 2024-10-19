639a91d682e4cefb70630693c55bae2692c9215b
ofektom <okpohot@gmail.com>
2024-10-20 00:20:46 +0100

    Initial commit

---
+++ src/main/java/com/ofektom/dlrsystem/DlrsystemApplication.java
@@ -0,0 +1,13 @@
+package com.ofektom.dlrsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlrsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlrsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ofektom/dlrsystem/DlrsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofektom.dlrsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlrsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
